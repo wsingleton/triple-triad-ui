81f8267063a22103952e431b95ae346ab261c898
wezley <wezley.singleton@gmail.com>
2021-03-28 00:08:46 -0400

    added input param to component to take in an array of Card objects (using dummy cards for now)

--- src/app/game/components/player-hand/player-hand.component.ts
+++ src/app/game/components/player-hand/player-hand.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Card } from '../../models/card';
 
 interface HandMetadata {
   hand: Object[];
@@ -12,7 +13,13 @@ interface HandMetadata {
 })
 export class PlayerHandComponent implements OnInit {
 
-  @Input() hand = new Array(5);
+  @Input() hand = [
+    new Card('jshhe93rhi', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png'),
+    new Card('ig538394hh', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png'),
+    new Card('g343545g45', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png'),
+    new Card('g46g4b544g', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png'),
+    new Card('ser43rewe4', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png')
+  ];
 
   metadata: HandMetadata = {
     hand: this.hand,
