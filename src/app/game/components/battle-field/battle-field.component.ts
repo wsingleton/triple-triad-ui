47878fb89de1eb5174ba51863caa3b8a4dee1ce8
wezley <wezley.singleton@gmail.com>
2021-03-28 00:07:23 -0400

    implemented a dummy array of cards for dev purposes (to be removed later)

--- src/app/game/components/battle-field/battle-field.component.ts
+++ src/app/game/components/battle-field/battle-field.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../../models/card';
 
 @Component({
   selector: 'battle-field',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./battle-field.component.scss']
 })
 export class BattleFieldComponent implements OnInit {
+
+  dummyCards = [
+    new Card('jshhe93rhi', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png', true),
+    new Card('ig538394hh', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png', true),
+    new Card('g343545g45', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png', true),
+    new Card('g46g4b544g', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png', true),
+    new Card('ser43rewe4', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png', true),
+    new Card('lkji8943ok', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png', true),
+    new Card('ky7n66645s', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png', true),
+    new Card('qwe9jj3fff', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png', true),
+    new Card('pooiuy7332', 'Elvoret', 7, 4, 8, 3, 'assets/cards/level-6/elvoret/elvoret-edited.png', true)
+  ];
 
   constructor() { }
 
