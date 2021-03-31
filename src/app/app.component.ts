81683d5592f5d7380e416ad0d986f7b5942a447c
wezley <wezley.singleton@gmail.com>
2021-03-30 20:21:46 -0400

    moved player hand declarations to app component for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Card } from './game/models/card';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'triple-triad';
+
+  opponentHand = [
+    new Card('jshhe93rhi', 'Geezard', 9, 10, 6, 2, 'assets/cards/level-1/g1_geezard-card.png', true, true),
+    new Card('ig538394hh', 'Funguar', 1, 4, 1, 5, 'assets/cards/level-1/g2_funguar-card.png', true, true),
+    new Card('g343545g45', 'Bite Bug', 9, 10, 6, 2, 'assets/cards/level-1/g3_bite-bug-card.png', true, true),
+    new Card('g46g4b544g', 'Red Bat', 5, 1, 1, 3, 'assets/cards/level-1/g11_cockatrice-card.png', true, true),
+    new Card('ser43rewe4', 'Caterchipillar', 1, 3, 3, 5, 'assets/cards/level-1/g10_caterchipillar-card.png', true, true)
+  ];
+
+  playerHand = [
+    new Card('ser43rewe4', 'Caterchipillar', 1, 3, 3, 5, 'assets/cards/level-1/g10_caterchipillar-card.png', true),
+    new Card('g46g4b544g', 'Red Bat', 5, 1, 1, 3, 'assets/cards/level-1/g11_cockatrice-card.png', true),
+    new Card('g343545g45', 'Bite Bug', 9, 10, 6, 2, 'assets/cards/level-1/g3_bite-bug-card.png', true),
+    new Card('jshhe93rhi', 'Geezard', 9, 10, 6, 2, 'assets/cards/level-1/g1_geezard-card.png', true),
+    new Card('ig538394hh', 'Funguar', 1, 4, 1, 5, 'assets/cards/level-1/g2_funguar-card.png', true)
+  ];
 }
