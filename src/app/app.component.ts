86809dba205245488abdb5bfdfdbe29670e587ac
wezley <wezley.singleton@gmail.com>
2021-03-31 00:45:28 -0400

    added all level 2 cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,15 @@ export class AppComponent {
     new Card('ig538394hh', 'Funguar', 1, 4, 1, 5, 'assets/cards/level-1/g2_funguar-card.png', true, true),
     new Card('g343545g45', 'Bite Bug', 9, 10, 6, 2, 'assets/cards/level-1/g3_bite-bug-card.png', true, true),
     new Card('g46g4b544g', 'Red Bat', 5, 1, 1, 3, 'assets/cards/level-1/g11_cockatrice-card.png', true, true),
-    new Card('ser43rewe4', 'Caterchipillar', 1, 3, 3, 5, 'assets/cards/level-1/g10_caterchipillar-card.png', true, true)
+    new Card('ser43rewe4', 'Caterchipillar', 1, 3, 3, 5, 'assets/cards/level-2/g21_jelleye-card.png', true, true)
   ];
 
   playerHand = [
-    new Card('ser43rewe4', 'Caterchipillar', 1, 3, 3, 5, 'assets/cards/level-1/g10_caterchipillar-card.png', true),
-    new Card('g46g4b544g', 'Red Bat', 5, 1, 1, 3, 'assets/cards/level-1/g11_cockatrice-card.png', true),
-    new Card('g343545g45', 'Bite Bug', 9, 10, 6, 2, 'assets/cards/level-1/g3_bite-bug-card.png', true),
-    new Card('jshhe93rhi', 'Geezard', 9, 10, 6, 2, 'assets/cards/level-1/g1_geezard-card.png', true),
-    new Card('ig538394hh', 'Funguar', 1, 4, 1, 5, 'assets/cards/level-1/g2_funguar-card.png', true)
+    new Card('ig538394hh', 'Grat', 1, 4, 1, 5, 'assets/cards/level-2/g12_grat-card.png', true),
+    new Card('ser43rewe4', 'Buel', 1, 3, 3, 5, 'assets/cards/level-2/g13_buel-card.png', true),
+    new Card('g46g4b544g', 'Mesmerize', 5, 1, 1, 3, 'assets/cards/level-2/g14_mesmerize-card.png', true),
+    new Card('g343545g45', 'Glacial Eye', 9, 10, 6, 2, 'assets/cards/level-2/g15_glacial-eye-card.png', true),
+    new Card('jshhe93rhi', 'Glacial Eye', 9, 10, 6, 2, 'assets/cards/level-2/g22_grand-mantis-card.png', true)
+    
   ];
 }
--- src/app/game/components/battle-field/battle-field.component.ts
+++ src/app/game/components/battle-field/battle-field.component.ts
@@ -9,15 +9,15 @@ import { Card } from '../../models/card';
 export class BattleFieldComponent implements OnInit {
 
   dummyCards = [
-    new Card('jshhe93rhi', 'Geezard', 9, 10, 6, 2, 'assets/cards/level-1/g1_geezard-card.png', true),
-    new Card('ig538394hh', 'Funguar', 1, 4, 1, 5, 'assets/cards/level-1/g2_funguar-card.png', true),
-    new Card('g343545g45', 'Bite Bug', 9, 10, 6, 2, 'assets/cards/level-1/g3_bite-bug-card.png', true),
-    new Card('g46g4b544g', 'Red Bat', 5, 1, 1, 3, 'assets/cards/level-1/g4_red-bat-card.png', true),
-    new Card('ser43rewe4', 'Blobra', 1, 3, 3, 5, 'assets/cards/level-1/g5_blobra-card.png', true),
-    new Card('lkji8943ok', 'Oilboyle', 1, 8, 4, 8, 'assets/cards/level-1/g6_gayla-card.png', true),
-    new Card('ky7n66645s', 'Oilboyle', 1, 8, 4, 8, 'assets/cards/level-1/g7_gesper-card.png', true),
-    new Card('qwe9jj3fff', 'Oilboyle', 1, 8, 4, 8, 'assets/cards/level-1/g8_fastitocalon-f-card.png', true),
-    new Card('pooiuy7332', 'Quistis', 1, 8, 4, 8, 'assets/cards/level-1/g9_blood-soul-card.png', true)
+    new Card('ig538394hh', 'Grat', 1, 4, 1, 5, 'assets/cards/level-2/g12_grat-card.png', true),
+    new Card('ser43rewe4', 'Buel', 1, 3, 3, 5, 'assets/cards/level-2/g13_buel-card.png', true),
+    new Card('g46g4b544g', 'Mesmerize', 5, 1, 1, 3, 'assets/cards/level-2/g14_mesmerize-card.png', true),
+    new Card('g343545g45', 'Glacial Eye', 9, 10, 6, 2, 'assets/cards/level-2/g15_glacial-eye-card.png', true),
+    new Card('ser43rewe4', 'Belhelmel', 1, 3, 3, 5, 'assets/cards/level-2/g16_belhelmel-card.png', true),
+    new Card('lkji8943ok', 'Thrustaevis', 1, 8, 4, 8, 'assets/cards/level-2/g17_thrustaevis-card.png', true),
+    new Card('ky7n66645s', 'Anacondaur', 1, 8, 4, 8, 'assets/cards/level-2/g18_anacondaur-card.png', true),
+    new Card('qwe9jj3fff', 'Creeps', 1, 8, 4, 8, 'assets/cards/level-2/g19_creeps-card.png', true),
+    new Card('pooiuy7332', 'Grendel', 1, 8, 4, 8, 'assets/cards/level-2/g20_grendel-card.png', true)
   ];
 
   constructor() { }
