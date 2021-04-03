d396e98e026c2a05d8409342be2d89b77451396d
wezley <wezley.singleton@gmail.com>
2021-04-03 00:10:40 -0400

    added in a null card value to a card space to test proper rendering of empty spaces (POC in place, still needs work)

--- src/app/game/components/battle-field/battle-field.component.ts
+++ src/app/game/components/battle-field/battle-field.component.ts
@@ -17,7 +17,8 @@ export class BattleFieldComponent {
     new Card('lkji8943ok', 'Ochu', 1, 8, 4, 8, 'assets/cards/level-3/g28_ochu-card.png', true),
     new Card('ky7n66645s', 'SAM08G', 1, 8, 4, 8, 'assets/cards/level-3/g29_sam08g-card.png', true),
     new Card('qwe9jj3fff', 'Death Claw', 1, 8, 4, 8, 'assets/cards/level-3/g30_death-claw-card.png', true),
-    new Card('pooiuy7332', 'Cactuar', 1, 8, 4, 8, 'assets/cards/level-3/g31_cactuar-card.png', true)
+    // new Card('pooiuy7332', 'Cactuar', 1, 8, 4, 8, 'assets/cards/level-3/g31_cactuar-card.png', true)
+    null
   ];
 
 }
