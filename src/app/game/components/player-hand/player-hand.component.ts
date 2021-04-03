4dbff431cbaf933ce5bd7e21f4921a69ad5904ea
wezley <wezley.singleton@gmail.com>
2021-04-03 13:32:17 -0400

    removed unused logic

--- src/app/game/components/player-hand/player-hand.component.ts
+++ src/app/game/components/player-hand/player-hand.component.ts
@@ -1,11 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Card } from '../../../shared/models/card';
 
-interface HandMetadata {
-  hand: Object[];
-  spread: boolean;
-}
-
 @Component({
   selector: 'player-hand',
   templateUrl: './player-hand.component.html',
@@ -14,14 +9,5 @@ interface HandMetadata {
 export class PlayerHandComponent {
 
   @Input() hand!: Card[];
-
-  metadata!: HandMetadata;
-
-  ngOnInit(): void {
-    this.metadata = {
-      hand: this.hand,
-      spread: true
-    }
-  }
 
 }
