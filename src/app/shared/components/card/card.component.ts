8d2a2aaa032c272657da239805271064b61b9421
wezley <wezley.singleton@gmail.com>
2021-04-03 13:33:33 -0400

    updated card selection animation; now shifts right/left depending on if card is opponent or player (went with pure CSS instead of ng animations)

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,30 +1,7 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import { trigger, state, style, transition, animate, AnimationBuilder, query, animation, useAnimation, AnimationPlayer } from '@angular/animations';
+import { Component, Input } from '@angular/core';
+import { trigger, style, transition, animate } from '@angular/animations';
 import { Card } from '../../models/card';
 
-const selectPlayerCardAnimation = animation([
-  state('selected', style({
-    'transform': 'translateX(-25%)'
-  })),
-  transition('selected => unselected', animate('1s', style({
-    'transform': 'translateX(0%)'
-  }))),
-  transition('unselected => selected', animate('1s', style({
-    'transform': 'translateX(-25%)'
-  })))
-]);
-
-const selectOpponentCardAnimation = animation([
-  state('selected', style({
-    'transform': 'translateX(25%)'
-  })),
-  transition('selected => unselected', animate('1s', style({
-    'transform': 'translateX(0%)'
-  }))),
-  transition('unselected => selected', animate('1s', style({
-    'transform': 'translateX(25%)'
-  })))
-]);
 
 @Component({
   selector: 'card[cardData]',
@@ -32,36 +9,21 @@ const selectOpponentCardAnimation = animation([
   styleUrls: ['./card.component.scss'],
   animations: [
     trigger('cardAnimation', [
-      state('selected', style({
-        'transform': 'translateX(25%)'
-      })),
       transition('default => stolen', animate('500ms', style({
         'transform': 'rotateY(360deg)'
       }))),
       transition('stolen => default', animate('500ms', style({
         'transform': 'rotateY(360deg)'
-      }))),
-      transition('selected => unselected', animate('250ms', style({
-        'transform': 'translateX(0%)'
-      }))),
-      transition('unselected => selected', animate('250ms', style({
-        'transform': 'translateX(25%)'
       })))
     ])
   ]
 })
-export class CardComponent implements AfterViewInit {
+export class CardComponent {
 
   @Input() cardData: Card | null;
-  @ViewChild('card') cardElement!: ElementRef;
-  animationPlayer!: AnimationPlayer;
 
-  constructor(private animationBuilder: AnimationBuilder) {
+  constructor() {
     this.cardData = null;
-  }
-
-  ngAfterViewInit(): void {
-    console.log(this.cardElement);
   }
 
   flipCard(): void {
@@ -79,25 +41,6 @@ export class CardComponent implements AfterViewInit {
     if (!this.cardData || this.cardData.inPlay) {
       return;
     }
-
-    // if (this.cardData?.isStolen) {
-    //   console.log(this.cardElement);
-    //   this.animationPlayer = this.animationBuilder.build([
-    //     useAnimation(selectOpponentCardAnimation)
-    //   ]).create(this.cardElement.nativeElement);
-    // } else {
-    //   console.log(this.cardElement);
-    //   this.animationPlayer = this.animationBuilder.build([
-    //     useAnimation(selectPlayerCardAnimation)
-    //   ]).create(this.cardElement.nativeElement);
-    // }
-
-    // this.animationPlayer.onDone(() => {
-    //   console.log('animation complete!');
-    //   this.animationPlayer.destroy();
-    // });
-
-    // this.animationPlayer.play();
 
     this.cardData.isSelected = !this.cardData.isSelected;
     
