58f556dee33155f163cdcac47ed5c77ade718850
wezley <wezley.singleton@gmail.com>
2021-04-03 21:51:31 -0400

    bound player-hand and battle-field inputs to observables that are fetched from gameService

--- src/app/game/components/game/game.component.ts
+++ src/app/game/components/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { Card } from '../../../shared/models/card';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'game',
@@ -8,21 +10,14 @@ import { Card } from '../../../shared/models/card';
 })
 export class GameComponent {
 
-  opponentHand = [
-    new Card('jshhe93rhi', 'Geezard', 9, 10, 6, 2, 'assets/cards/level-1/g1_geezard-card.png', false, true),
-    new Card('ig538394hh', 'Funguar', 1, 4, 1, 5, 'assets/cards/level-1/g2_funguar-card.png', false, true),
-    new Card('g343545g45', 'Bite Bug', 9, 10, 6, 2, 'assets/cards/level-1/g3_bite-bug-card.png', false, true),
-    new Card('g46g4b544g', 'Red Bat', 5, 1, 1, 3, 'assets/cards/level-1/g11_cockatrice-card.png', false, true),
-    new Card('ser43rewe4', 'Tonberry', 1, 3, 3, 5, 'assets/cards/level-3/g32_tonberry-card.png', false, true)
-  ];
+  $fieldCards!: Observable<Card[]>;
+  $opponentHand!: Observable<Card[]>;
+  $playerHand!: Observable<Card[]>;
 
-  playerHand = [
-    new Card('ig538394hh', 'Grat', 1, 4, 1, 5, 'assets/cards/level-2/g12_grat-card.png'),
-    new Card('ser43rewe4', 'Buel', 1, 3, 3, 5, 'assets/cards/level-2/g13_buel-card.png'),
-    new Card('g46g4b544g', 'Mesmerize', 5, 1, 1, 3, 'assets/cards/level-2/g14_mesmerize-card.png'),
-    new Card('g343545g45', 'Glacial Eye', 9, 10, 6, 2, 'assets/cards/level-2/g15_glacial-eye-card.png'),
-    new Card('pooiuy7332', 'Abyss Worm', 1, 8, 4, 8, 'assets/cards/level-3/g33_abyss-worm-card.png')
-    
-  ];
+  constructor(private gameService: GameService) {
+    this.$fieldCards = this.gameService.$fieldCards;
+    this.$opponentHand = this.gameService.$opponentHand;
+    this.$playerHand = this.gameService.$playerHand;
+  }
 
 }
