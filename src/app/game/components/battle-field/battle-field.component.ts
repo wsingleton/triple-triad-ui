6da85163ee0f7651c4bb7001f101c675e5ad4cf0
wezley <wezley.singleton@gmail.com>
2021-04-03 21:55:40 -0400

    game play logic in place

--- src/app/game/components/battle-field/battle-field.component.ts
+++ src/app/game/components/battle-field/battle-field.component.ts
@@ -1,24 +1,194 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CardSpaceComponent } from 'src/app/shared/components/card-space/card-space.component';
 import { Card } from '../../../shared/models/card';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'battle-field',
   templateUrl: './battle-field.component.html',
   styleUrls: ['./battle-field.component.scss']
 })
-export class BattleFieldComponent {
+export class BattleFieldComponent implements OnInit {
 
-  dummyCards = [
-    new Card('ig538394hh', 'Forbidden', 1, 4, 1, 5, 'assets/cards/level-3/g23_forbidden-card.png', true),
-    new Card('ser43rewe4', 'Armadodo', 1, 3, 3, 5, 'assets/cards/level-3/g24_armadodo-card.png', true),
-    new Card('g46g4b544g', 'Tri-Face', 5, 1, 1, 3, 'assets/cards/level-3/g25_tri-face-card.png', true),
-    new Card('g343545g45', 'Fastitocalon', 9, 10, 6, 2, 'assets/cards/level-3/g26_fastitocalon-card.png', true),
-    new Card('ser43rewe4', 'Snow Lion', 1, 3, 3, 5, 'assets/cards/level-3/g27_snow-lion-card.png', true),
-    new Card('lkji8943ok', 'Ochu', 1, 8, 4, 8, 'assets/cards/level-3/g28_ochu-card.png', true),
-    new Card('ky7n66645s', 'SAM08G', 1, 8, 4, 8, 'assets/cards/level-3/g29_sam08g-card.png', true),
-    new Card('qwe9jj3fff', 'Death Claw', 1, 8, 4, 8, 'assets/cards/level-3/g30_death-claw-card.png', true),
-    // new Card('pooiuy7332', 'Cactuar', 1, 8, 4, 8, 'assets/cards/level-3/g31_cactuar-card.png', true)
-    null
-  ];
+  @Input() $fieldCards!: Observable<Card[]>;
+  fieldCards!: Card[];
+
+  @ViewChildren(CardSpaceComponent) cardSpaces!: QueryList<CardSpaceComponent>;
+
+  constructor(private gameService: GameService) { }
+
+  ngOnInit(): void {
+    this.$fieldCards.subscribe(next => this.fieldCards = next);
+  }
+
+  getCardPosition($event: Event) {
+    this.gameService.updateDestinationIndex(+(<HTMLElement>$event.currentTarget)?.id);
+  }
+
+  flipCard(cardSpace: CardSpaceComponent) {
+    cardSpace.flipCard();
+  }
+
+  detectChange(cardSpace: CardSpaceComponent) {
+    console.log('FIELD CHANGE DETECTED!');
+    console.log(this.cardSpaces);
+    console.log(cardSpace);
+
+    let card = cardSpace.cardData!;
+    let cardPosition = cardSpace.currentPosition;
+    let shouldFlip: boolean;
+    switch (cardPosition) {
+      case 0:
+        shouldFlip = this.shouldFlipCardBelow(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 3].flipCard();
+
+        shouldFlip = this.shouldFlipCardToRight(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 1].flipCard();
+
+        break;
+
+      case 1:
+        shouldFlip = this.shouldFlipCardToLeft(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 1].flipCard();
+
+        shouldFlip = this.shouldFlipCardToRight(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 1].flipCard();
+
+        shouldFlip = this.shouldFlipCardBelow(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 3].flipCard();
+
+        break;
+
+      case 2:
+        shouldFlip = this.shouldFlipCardBelow(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 3].flipCard();
+        
+        shouldFlip = this.shouldFlipCardToLeft(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 1].flipCard();
+
+        break;
+
+      case 3:
+        shouldFlip = this.shouldFlipCardAbove(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 3].flipCard();
+        
+        shouldFlip = this.shouldFlipCardToRight(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 1].flipCard();
+
+        shouldFlip = this.shouldFlipCardBelow(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 3].flipCard();
+
+        break;
+
+      case 4:
+        shouldFlip = this.shouldFlipCardAbove(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 3].flipCard();
+
+        shouldFlip = this.shouldFlipCardToLeft(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 1].flipCard();
+
+        shouldFlip = this.shouldFlipCardToRight(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 1].flipCard();
+
+        shouldFlip = this.shouldFlipCardBelow(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 3].flipCard();
+
+        break;
+      case 5:
+        shouldFlip = this.shouldFlipCardAbove(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 3].flipCard();
+        
+        shouldFlip = this.shouldFlipCardToLeft(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 1].flipCard();
+
+        shouldFlip = this.shouldFlipCardBelow(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 3].flipCard();
+
+        break;
+
+      case 6:
+        shouldFlip = this.shouldFlipCardAbove(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 3].flipCard();
+
+        shouldFlip = this.shouldFlipCardToRight(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 1].flipCard();
+
+        break;
+      case 7:
+        shouldFlip = this.shouldFlipCardAbove(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 3].flipCard();
+
+        shouldFlip = this.shouldFlipCardToLeft(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 1].flipCard();
+
+        shouldFlip = this.shouldFlipCardToRight(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition + 1].flipCard();
+
+        break;
+      case 8:
+        shouldFlip = this.shouldFlipCardAbove(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 3].flipCard();
+
+        shouldFlip = this.shouldFlipCardToLeft(card, cardPosition);
+        if (shouldFlip) this.cardSpaces['_results'][cardPosition - 1].flipCard();
+
+        break;
+    }
+  }
+
+  private shouldFlipCardAbove(card: Card, position: number) {
+    
+    if (position < 2) return false;
+
+    let compCard = this.fieldCards[position - 3];
+    if (!compCard) return false;
+
+    let sameOwner = card.owner == compCard.owner;
+    if (sameOwner) return false;
+
+    console.log(`Comparing ${card.name} to ${compCard.name}`);
+    return card.topRank > compCard.bottomRank;
+
+  }
+
+  private shouldFlipCardBelow(card: Card, position: number) {
+    if (position > 5) return false;
+
+    let compCard = this.fieldCards[position + 3];
+    if (!compCard) return false;
+
+    let sameOwner = card.owner == compCard.owner;
+    if (sameOwner) return false;
+
+    console.log(`Comparing ${card.name} to ${compCard.name}`);
+    return card.bottomRank > compCard.topRank;
+  }
+
+  private shouldFlipCardToLeft(card: Card, position: number) {
+    if (position == 0 || position == 3 || position == 6) return false;
+
+    let compCard = this.fieldCards[position - 1];
+    if (!compCard) return false;
+
+    let sameOwner = card.owner == compCard.owner;
+    if (sameOwner) return false;
+
+    console.log(`Comparing ${card.name} to ${compCard.name}`);
+    return card.leftRank > compCard.rightRank;
+  }
+
+  private shouldFlipCardToRight(card: Card, position: number) {
+    if (position == 2 || position == 5 || position == 8) return false;
+
+    let compCard = this.fieldCards[position + 1];
+    if (!compCard) return false;
+
+    let sameOwner = card.owner == compCard.owner;
+    if (sameOwner) return false;
+
+    console.log(`Comparing ${card.name} to ${compCard.name}`);
+    return card.rightRank > compCard.leftRank;
+  }
 
 }
