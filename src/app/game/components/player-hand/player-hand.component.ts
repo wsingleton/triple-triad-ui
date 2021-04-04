6fc1ef4dd6e33d6e4db9d0ae1795d0df22653d6e
wezley <wezley.singleton@gmail.com>
2021-04-03 21:55:27 -0400

    fetching hand from gameService; added methods to call down to child components

--- src/app/game/components/player-hand/player-hand.component.ts
+++ src/app/game/components/player-hand/player-hand.component.ts
@@ -1,13 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CardSpaceComponent } from 'src/app/shared/components/card-space/card-space.component';
 import { Card } from '../../../shared/models/card';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'player-hand',
   templateUrl: './player-hand.component.html',
   styleUrls: ['./player-hand.component.scss']
 })
-export class PlayerHandComponent {
+export class PlayerHandComponent implements OnInit {
 
-  @Input() hand!: Card[];
+  @Input() $hand!: Observable<Card[]>;
+  hand!: Card[];
 
+  constructor(private gameService: GameService) { }
+
+  ngOnInit(): void {
+    this.$hand.subscribe(next => this.hand = next);
+  }
+  
+  peekCard(cardSpace: CardSpaceComponent) {
+    cardSpace.peekCard();
+  }
+
+  selectCard(cardSpace: CardSpaceComponent) {
+
+    if (cardSpace.metadata.containsCard){
+      this.gameService.addCardToField({
+        index: +(<any>cardSpace)['__ngContext__'][13][0].id,
+        card: <Card>cardSpace.cardData,
+        owner: cardSpace.cardData?.isStolen ? 'opponent' : 'player'
+      });
+    }
+
+  }
 }
