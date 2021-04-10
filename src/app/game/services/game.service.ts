3019c87c8621ed2cfeb2878b0852742ddcbeb56e
wezley <wezley.singleton@gmail.com>
2021-04-10 19:30:07 -0400

    setting up game-service to use game-state

--- src/app/game/services/game.service.ts
+++ src/app/game/services/game.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { CardSpace } from 'src/app/shared/models/card-space';
 import { Element } from 'src/app/shared/models/element';
+import { Player } from 'src/app/shared/models/player';
 import { Card } from '../../shared/models/card';
+import { GameState } from '../state/game.state';
 
 interface CardMetadata {
   index: number,
@@ -14,6 +17,9 @@ interface CardMetadata {
   providedIn: 'root'
 })
 export class GameService {
+
+  private gameState: BehaviorSubject<GameState>;
+  gameState$: Observable<GameState>;
 
   private fieldCardsSource: BehaviorSubject<(Card | null)[]>;
   fieldCards$: Observable<(Card | null)[]>;
@@ -27,6 +33,40 @@ export class GameService {
   private destinationIndexSource: BehaviorSubject<number>;
 
   constructor(private httpClient: HttpClient) {
+
+    let opponent = new Player('Seifer', 10, 0, 0, 0);
+    let player = new Player('Squall', 10, 0, 0, 0);
+
+    this.gameState = new BehaviorSubject<GameState>({
+      gameId: 1,
+      field: [
+        new CardSpace('f-0'), new CardSpace('f-1'), new CardSpace('f-2'),
+        new CardSpace('f-3'), new CardSpace('f-4'), new CardSpace('f-5'),
+        new CardSpace('f-6'), new CardSpace('f-7'), new CardSpace('f-8'),
+      ],
+      opponentHand: [
+        new CardSpace('o-0', Element.NONE, new Card('l01.g001.n', 'Geezard', 1, 4, 1, 5, 'assets/cards/level-1/g1_geezard-card.png')),
+        new CardSpace('o-1', Element.NONE, new Card('l01.g002.n', 'Funguar', 5, 1, 1, 3, 'assets/cards/level-1/g2_funguar-card.png')),
+        new CardSpace('o-2', Element.NONE, new Card('l01.g003.n', 'Bite Bug', 1, 3, 3, 5, 'assets/cards/level-1/g3_bite-bug-card.png')),
+        new CardSpace('o-3', Element.NONE, new Card('l01.g004.n', 'Red Bat', 6, 1, 1, 2, 'assets/cards/level-1/g4_red-bat-card.png')),
+        new CardSpace('o-4', Element.NONE, new Card('l01.g005.n', 'Blobra', 2, 3, 1, 5, 'assets/cards/level-1/g5_blobra-card.png'))
+      ],
+      playerHand: [
+        new CardSpace('p-0', Element.NONE, new Card('l03.g029.f', 'SAM08G', 5, 6, 2, 4, 'assets/cards/level-3/g29_sam08g-card.png', Element.FIRE)),
+        new CardSpace('p-1', Element.NONE, new Card('l03.g030.f', 'Death Claw', 4, 4, 7, 2, 'assets/cards/level-3/g30_death-claw-card.png', Element.FIRE)),
+        new CardSpace('p-2', Element.NONE, new Card('l03.g031.n', 'Cactuar', 6, 2, 6, 3, 'assets/cards/level-3/g31_cactuar-card.png')),
+        new CardSpace('p-3', Element.NONE, new Card('l03.g032.n', 'Tonberry', 3, 6, 4, 4, 'assets/cards/level-3/g32_tonberry-card.png')),
+        new CardSpace('p-4', Element.NONE, new Card('l03.g033.e', 'Abyss Worm', 7, 2, 3, 5, 'assets/cards/level-3/g33_abyss-worm-card.png', Element.EARTH))
+      ],
+      players: [
+        opponent,
+        player
+      ],
+      turnOwner: opponent,
+      winner: undefined
+    });
+
+    this.gameState$ = this.gameState.asObservable();
     
     this.fieldCardsSource = new BehaviorSubject<(Card | null)[]>(new Array(9));
     this.fieldCards$ = this.fieldCardsSource.asObservable();
@@ -75,7 +115,6 @@ export class GameService {
   updateDestinationIndex(index: number): void {
     this.destinationIndexSource.next(index);
   }
-
 
   addCardToField(cardMetadata: CardMetadata) {
 
